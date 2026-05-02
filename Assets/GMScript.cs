f6227291f1348ce73c9ec65fd1dfcfea851c8fc3
Alvin <Alv1nC03111219@gmail.com>
2026-05-02 13:02:48 -0400

    a

--- Assets/GMScript.cs
+++ Assets/GMScript.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 
 public class GMScript : MonoBehaviour
 {
-    public GameObject startScreenUI;
-    public GameObject player;
 
     void Start()
     {
-        player.SetActive(false);
-        Time.timeScale = 0f;
-    }
 
-    public void StartGame()
-    {
-        startScreenUI.SetActive(false); // hide menu
-        player.SetActive(true);         // enable gameplay
-        Time.timeScale = 1f;            // resume game
     }
 }
--- Assets/SceneSwitcher.cs
+++ Assets/SceneSwitcher.cs
@@ -5,15 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
