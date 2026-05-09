f27e7a241c97b720dd66586a53b35a560b1d7089
taneeez <zeenatadeboye.ga@gmail.com>
2026-05-09 11:25:55 -0400

    Dialouge

--- Assets/GMScript.cs
+++ Assets/GMScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GMScript : MonoBehaviour
 {
@@ -8,16 +9,42 @@ public class GMScript : MonoBehaviour
     public int score;
     public GameObject chest;
 
+    public GameObject namePanel;
+    public GameObject dialougePanel;
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+
+        namePanel.SetActive(true);
+        textComponent.text = string.Empty;
+        dialougePanel.SetActive(true);
+        StartDialouge();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+
        if (score == 0) //box with code activated - after user gets remote. score +=1
         {
 
@@ -32,5 +59,37 @@ public class GMScript : MonoBehaviour
 
         }
         
+    }
+
+    void StartDialouge()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            namePanel.SetActive(false);
+            dialougePanel.SetActive(false);
+        }
     }
 }
